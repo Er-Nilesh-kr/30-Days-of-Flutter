7fd7f905eca0af15fb987d8deeda778e04e0e9bc
Nilesh <allmailtonilesh@gmail.com>
2021-02-23 22:03:01 +0530

    Add items

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -6,17 +7,63 @@ class MyDrawer extends StatelessWidget {
     final imageUrl =
         "https://avatars.githubusercontent.com/u/70195632?s=400&u=deba1a00c6cd2898a0cd8a89463559f64a894d31&v=4";
     return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
               padding: EdgeInsets.zero,
               child: UserAccountsDrawerHeader(
                 margin: EdgeInsets.zero,
                 accountName: Text("Nilesh Kumar"),
                 accountEmail: Text("allmailtonilesh@gmail.com"),
-                currentAccountPicture: Image.network(imageUrl),
-              ))
-        ],
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.3,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.3,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Contact",
+                textScaleFactor: 1.3,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
