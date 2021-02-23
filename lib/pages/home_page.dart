659ce67ba921cfabe1f881838a16439a51c689f1
Nilesh <allmailtonilesh@gmail.com>
2021-02-23 16:13:53 +0530

    Add drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -14,7 +15,7 @@ class HomePage extends StatelessWidget {
           child: Text("Welcome to MyApp $num $str"),
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
