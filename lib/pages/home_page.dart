df0b79cdcbd0960463abbdd7c67f802d48881ee2
Nilesh <allmailtonilesh@gmail.com>
2021-02-26 00:59:15 +0530

    Day-10 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/Model/catalog.dart';
 import 'package:myapp/widgets/drawer.dart';
+import 'package:myapp/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final int num = 101;
-    final String str = "Hello World!";
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to MyApp $num $str"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
