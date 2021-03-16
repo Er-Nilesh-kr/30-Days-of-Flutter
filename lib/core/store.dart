ff1c8f014fdb0744ff960edc195febfb4340c507
Nilesh <allmailtonilesh@gmail.com>
2021-03-16 08:04:32 +0530

    Day-24 Completed

--- lib/core/store.dart
+++ lib/core/store.dart
@@ -1,12 +1,13 @@
 import 'package:myapp/Model/cart.dart';
 import 'package:myapp/Model/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class Mystore extends VxStore {
   CatalogModel catalog;
   CartModel cart;
-  Mystore(){
+  Mystore() {
     catalog = CatalogModel();
     cart = CartModel();
     cart.catalog = catalog;
-  };
+  }
 }
